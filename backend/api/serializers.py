45ac6cd666fd93fe7d29fba2e725cd8fa92a74c2
Nanostack-Technologies <info.nanostack@gmail.com>
2026-02-06 04:09:22 +0530

    Update serializers: Save modifications to GitHub

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,18 +2,34 @@ from rest_framework import serializers
 from .models import User, Product, RetailerProductPrice, Order, OrderItem
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
+    
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'role', 'phone', 'distributor_link']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['id', 'username', 'email', 'role', 'phone', 'distributor_link', 'password', 'is_active']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'is_active': {'default': True}
+        }
     
     def create(self, validated_data):
         password = validated_data.pop('password', None)
+        # Ensure user is active by default
+        validated_data['is_active'] = validated_data.get('is_active', True)
         user = self.Meta.model(**validated_data)
         if password:
             user.set_password(password)
         user.save()
         return user
+    
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
